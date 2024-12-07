528f0eebedf7cc9583581623a32e83d9b97d86c8
William Navarro <142845725+WilliamNavarro06@users.noreply.github.com>
2024-12-06 22:13:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿for(int i = 0; i <= 101; i++){
+    if(i % 4 == 0){
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("PIM");
+        Console.ResetColor();
+    }
+    else{
+        Console.WriteLine(i);
+    }
+}
